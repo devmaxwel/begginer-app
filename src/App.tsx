a4f04682c58e6568b0bf0d983b6ae3543dd3499d
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-01-06 14:31:02 +0300

    last pasrt

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React,{FC} from 'react';
 import './App.css';
-import { Person } from './components/Person';
+import { Person, hairColor } from './components/Person';
 
+interface appContextInterface{
+  name:String;
+  age:Number;
+  country: String;
+}
+
+const appContext = React.createContext<appContextInterface| null>(null);
+
+const AppcontextProvilder=(props:any): any=> {
+
+     const contextValue:appContextInterface = {
+       name:"Maxwel",
+       age:30,
+       country:"Ghana"
+     }
+
+     return  <appContext.Provider value={{}}>
+              {props.children}
+       </appContext.Provider>
+     
+}
+
+const useAppContext=()=> {
+
+  return React.useContext(appContext);
+}
 
 const  App: FC =() => {
 
   return (
     <div className="App">
 
-        <Person name="Maxwel" age={21} email="max@gmail.com" />
+          <AppcontextProvilder>
+              <Person name="Maxwel" age={21} 
+              email="max@gmail.com" 
+              hairColor={hairColor.Pink} />
+         </AppcontextProvilder>
 
     </div>
   );
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,16 +1,31 @@
+import { type } from 'os';
 import * as React from 'react';
 
 // We use interfaces to define type props
+export enum hairColor{
+  Blonde ="wow, You're a blode",
+  Brown = "Nice hair",
+  Pink = "Good for you"
+
+};
 
 interface AppProps {
   name:String;
   age:Number;
   email:String;
+  hairColor: hairColor
 
 }
+// enumas are used if you have many options to choose from 
 
-export const Person:React.FC<AppProps> =({name, age,email})  => {
+
+export const Person:React.FC<AppProps> =({name, age,email, hairColor})  => {
   const [country, setCountry] = React.useState<String>("");
+
+  // Type is used to restrict a certain const to specific values
+
+  type NameType = "Maxwel" | "Ochieng" | "Okeyo" | "Bildad"
+  const nameForType: NameType =  "Bildad"
 
   const handleChange=(event:React.ChangeEvent<HTMLInputElement>)=>{
     setCountry(event.target.value)
@@ -22,9 +37,11 @@ export const Person:React.FC<AppProps> =({name, age,email})  => {
           <h1>{name}</h1>
           <h1>{age}</h1>
           <h1>{email}</h1>
+          {hairColor}    
+          <br />
           <input type="text" placeholder="Enter Country" onChange={handleChange} />
-          {country}
-          
+          {country} 
+        
             
         </div>
   );
